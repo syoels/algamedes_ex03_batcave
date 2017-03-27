64dd4792aa570ae828cdfccd109b367ee39daa64
Yoel S <syoels@gmail.com>
2017-03-27 21:54:34 +0300

    Add assets

---
+++ Assets/Scripts/batcave/spline/ContinuousSpline.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace BatCave.Spline {
+public class ContinuousSpline : SingleSpline {
+    public ContinuousSpline(Vector2[] firstControlPoints) : base(firstControlPoints) {
+        // Empty on purpose.
+    }
+
+    /// <summary>
+    /// Continue the spline with more control points.
+    /// </summary>
+    public void AddControlPoints(params Vector2[] points) {
+        // TODO: Implement: don't keep all of the control points, there could be
+        //       millions of them!
+    }
+}
+}
---
+++ Assets/Scripts/batcave/spline/SingleSpline.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace BatCave.Spline {
+public class SingleSpline {
+    public SingleSpline(Vector2[] controlPoints) {
+        // TODO: Implement: initialize the spline to match the given control points.
+    }
+
+    /// <summary>
+    /// Returns the value of the spline at point X.
+    /// </summary>
+    public float Value(float X) {
+        // TODO: Implement: find the polynom f_i that passes at X and calculate
+        //       f_i(X).
+        return 0f;
+    }
+}
+}
